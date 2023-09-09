1d75c63d97f27bcb06d402662b7133ad55ec7247
Calvin <nicxon.calvin@gmail.com>
2023-09-09 20:18:45 +0100

    fix bug on renaming columns

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -153,9 +153,9 @@ data_merged.columns = [
     "gyr_x",
     "gyr_y",
     "gyr_z",
+    "participant",
     "label",
     "category",
-    "participant",
     "set",
 ]
 
@@ -173,9 +173,9 @@ sampling = {
     "gyr_x": "mean",
     "gyr_y": "mean",
     "gyr_z": "mean",
+    "participant": "last",
     "label": "last",
     "category": "last",
-    "participant": "last",
     "set": "last",
 }
 
