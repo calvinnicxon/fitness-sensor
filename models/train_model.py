95c45bdd96fed06099e6c2969aefa822c818433b
Calvin <nicxon.calvin@gmail.com>
2023-09-13 16:08:04 +0100

    create training and test set

--- models/train_model.py
+++ models/train_model.py
@@ -6,6 +6,7 @@ from LearningAlgorithms import ClassificationAlgorithms
 import seaborn as sns
 import itertools
 from sklearn.metrics import accuracy_score, confusion_matrix
+import os
 
 
 # Plot settings
@@ -14,11 +15,30 @@ plt.rcParams["figure.figsize"] = (20, 5)
 plt.rcParams["figure.dpi"] = 100
 plt.rcParams["lines.linewidth"] = 2
 
+os.getcwd()
+df = pd.read_pickle("../data/interim/03_data_features.pkl")
 
 # --------------------------------------------------------------
 # Create a training and test set
 # --------------------------------------------------------------
 
+df_train = df.drop(["participant", "category", "set", "duration"], axis=1)
+
+X = df_train.drop("label", axis=1)
+y = df_train["label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=42, stratify=y
+)
+
+fig, ax = plt.subplots(figsize=(10, 5))
+df_train["label"].value_counts().plot(
+    kind="bar", ax=ax, color="lightblue", label="Total"
+)
+y_train.value_counts().plot(kind="bar", ax=ax, color="dodgerblue", label="Train")
+y_test.value_counts().plot(kind="bar", ax=ax, color="royalblue", label="Test")
+plt.legend()
+plt.show()
 
 # --------------------------------------------------------------
 # Split feature subsets
